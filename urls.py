e98dd579d42195899d5bc7ddcb9f45095a4ee478
sskarthik04 <137642757+sskarthik04@users.noreply.github.com>
2023-07-02 18:38:45 +0530

    first commit project

--- urls.py
+++ urls.py
@@ -1,34 +1,14 @@
-"""shopkart URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    #1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('',views.home , name='home'),
-    path('logout',views.logout_page , name='logout'),
-    path('login',views.login_page , name='login'),
-    path('register',views.register , name='register'),
-    path('cart',views.cart_page , name='cart'),
-    path('fav',views.fav_page , name='fav'),
-    path('favviewpage',views.favviewpage , name='favviewpage'),
-    path('remove_cart/<str:cid>',views.remove_cart , name='remove_cart'),
-    path('remove_fav/<str:fid>',views.remove_fav , name='remove_fav'),
-    path('collections',views.collections , name='collections'),
-    path('collections/<str:name>' ,views.collectionsview , name='collections'),
-    path('collections/<str:cname>/<str:pname>' ,views.product_details , name='product_details'),
-    path('addtocart',views.add_to_cart , name='addtocart'),
+    path('admin/', admin.site.urls),
+    path('', include('shop.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
